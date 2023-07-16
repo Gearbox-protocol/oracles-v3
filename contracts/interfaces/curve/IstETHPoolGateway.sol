fee03d7da5a41b39056857b571cc961daffda6fc
Dmitry Lekhovitsky <lekhovitsky@gmail.com>
2023-07-16 16:20:15 +0300

    feat: remove curve stETH pool gateway
    Oracles themselves do not depend on gateway implementation at all.
    Gateway is only queried once in price feed deployer to learn address of the pool.

---
+++ contracts/interfaces/curve/IstETHPoolGateway.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+// Gearbox Protocol. Generalized leverage for DeFi protocols
+// (c) Gearbox Holdings, 2023
+pragma solidity ^0.8.17;
+
+interface IstETHPoolGateway {
+    function pool() external view returns (address);
+}
