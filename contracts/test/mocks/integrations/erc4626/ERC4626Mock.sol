fca08e9ba3829c57e6abde1dbd8d98fb9ac0c075
Dmitry Lekhovitsky <lekhovitsky@gmail.com>
2023-07-26 18:14:27 +0300

    fix: cleanup `ERC4626PriceFeed` + unit tests

---
+++ contracts/test/mocks/integrations/erc4626/ERC4626Mock.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: UNLICENSED
+// Gearbox Protocol. Generalized leverage for DeFi protocols
+// (c) Gearbox Foundation, 2023.
+pragma solidity ^0.8.17;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {ERC4626} from "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
+
+contract ERC4626Mock is ERC4626 {
+    uint256 public assetsPerShare;
+
+    constructor(address asset, string memory name, string memory symbol) ERC20(name, symbol) ERC4626(IERC20(asset)) {}
+
+    function convertToAssets(uint256 shares) public view override returns (uint256 assets) {
+        return shares * assetsPerShare / 10 ** 18;
+    }
+
+    function setAssetsPerShare(uint256 newAssetsPerShare) external {
+        assetsPerShare = newAssetsPerShare;
+    }
+}
