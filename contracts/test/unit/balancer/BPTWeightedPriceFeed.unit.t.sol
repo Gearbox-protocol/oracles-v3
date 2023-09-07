87b6f920b64f10b09998a2d677585e0d3db1e9cf
Dmitry Lekhovitsky <lekhovitsky@gmail.com>
2023-09-07 13:35:32 +0300

    test: more advanced `BPTWeightedPriceFeed` tests

--- contracts/test/unit/balancer/BPTWeightedPriceFeed.unit.t.sol
+++ contracts/test/unit/balancer/BPTWeightedPriceFeed.unit.t.sol
@@ -13,12 +13,12 @@ import {PriceFeedMock} from "@gearbox-protocol/core-v3/contracts/test/mocks/orac
 import {IBalancerVault} from "../../../interfaces/balancer/IBalancerVault.sol";
 import {IBalancerWeightedPool} from "../../../interfaces/balancer/IBalancerWeightedPool.sol";
 import {PriceFeedParams} from "../../../oracles/PriceFeedParams.sol";
-import {BPTWeightedPriceFeed} from "../../../oracles/balancer/BPTWeightedPriceFeed.sol";
+import {BPTWeightedPriceFeedHarness} from "./BPTWeightedPriceFeed.harness.sol";
 
 import {ZeroAddressException} from "@gearbox-protocol/core-v3/contracts/interfaces/IExceptions.sol";
 
-contract BPTWeightedPriceFeedUnitTest is PriceFeedUnitTest {
-    BPTWeightedPriceFeed priceFeed;
+contract BPTWeightedPriceFeedHarnessUnitTest is PriceFeedUnitTest {
+    BPTWeightedPriceFeedHarness priceFeed;
     BalancerVaultMock balancerVault;
     BalancerWeightedPoolMock balancerPool;
 
@@ -42,7 +42,7 @@ contract BPTWeightedPriceFeedUnitTest is PriceFeedUnitTest {
         _setupBalancerMocks(8);
 
         vm.expectRevert(ZeroAddressException.selector);
-        new BPTWeightedPriceFeed(
+        new BPTWeightedPriceFeedHarness(
             address(addressProvider),
             address(0),
             address(balancerPool),
@@ -50,7 +50,7 @@ contract BPTWeightedPriceFeedUnitTest is PriceFeedUnitTest {
         );
 
         vm.expectRevert(ZeroAddressException.selector);
-        new BPTWeightedPriceFeed(
+        new BPTWeightedPriceFeedHarness(
             address(addressProvider),
             address(balancerVault),
             address(0),
@@ -89,6 +89,87 @@ contract BPTWeightedPriceFeedUnitTest is PriceFeedUnitTest {
         }
     }
 
+    /// @notice U:[BAL-W-3]: Formulas compute correctly with different weights
+    function test_U_BAL_W_03_formulas_compute_correctly_with_different_weights() public {
+        // weights are 0.2, 0.3, and 0.5
+        uint256[] memory weights = new uint256[](3);
+        weights[0] = 0.2 ether;
+        weights[1] = 0.3 ether;
+        weights[2] = 0.5 ether;
+
+        // total supply of the pool is 1
+        balancerPool = new BalancerWeightedPoolMock("TEST_POOL", 1 ether, false, weights);
+
+        // token balances are 1, 0.5 and 0.25
+        PoolToken[] memory poolTokens = new PoolToken[](3);
+        poolTokens[0] = PoolToken(address(underlyings[0]), 1 ether);
+        poolTokens[1] = PoolToken(address(underlyings[1]), 0.5 ether / 10);
+        poolTokens[2] = PoolToken(address(underlyings[2]), 0.25 ether / 100);
+
+        balancerVault = new BalancerVaultMock();
+        balancerVault.hackPoolTokens("TEST_POOL", poolTokens);
+
+        // underlying token prices are $1, $4, and $16
+        priceFeed = _newBalancerPriceFeed(3);
+
+        assertApproxEqAbs(
+            priceFeed.getAggregatePrice(),
+            1697659589, // `(1 / 0.2) ** 0.2 * (4 / 0.3) ** 0.3 * (16 / 0.5) ** 0.5` with 8 decimals
+            1,
+            "Incorrect aggregate price"
+        );
+
+        assertApproxEqAbs(
+            priceFeed.getLPExchangeRate(),
+            406126198178117824, // `1 ** 0.2 * 0.5 ** 0.3 * 0.25 ** 0.5` with 18 decimals
+            1e6,
+            "Incorrect LP exchange rate"
+        );
+    }
+
+    /// @notice U:[BAL-W-4]: Sorting works as expected
+    function test_U_BAL_W_04_sorting_works_as_expected() public {
+        // weights are 0.5, 0.3, and 0.2
+        uint256[] memory weights = new uint256[](3);
+        weights[0] = 0.5 ether;
+        weights[1] = 0.3 ether;
+        weights[2] = 0.2 ether;
+
+        // total supply of the pool is 1
+        balancerPool = new BalancerWeightedPoolMock("TEST_POOL", 1 ether, false, weights);
+
+        // token balances are 1, 0.5 and 0.25
+        PoolToken[] memory poolTokens = new PoolToken[](3);
+        poolTokens[0] = PoolToken(address(underlyings[0]), 1 ether);
+        poolTokens[1] = PoolToken(address(underlyings[1]), 0.5 ether / 10);
+        poolTokens[2] = PoolToken(address(underlyings[2]), 0.25 ether / 100);
+
+        balancerVault = new BalancerVaultMock();
+        balancerVault.hackPoolTokens("TEST_POOL", poolTokens);
+
+        priceFeed = _newBalancerPriceFeed(3);
+
+        uint256[] memory expectedWeights = new uint256[](3);
+        expectedWeights[0] = 0.2 ether;
+        expectedWeights[1] = 0.3 ether;
+        expectedWeights[2] = 0.5 ether;
+        assertEq(priceFeed.getWeightsArrayExposed(), expectedWeights, "Incorrect weights");
+
+        uint256[] memory expectedBalances = new uint256[](3);
+        expectedBalances[0] = 0.25 ether;
+        expectedBalances[1] = 0.5 ether;
+        expectedBalances[2] = 1 ether;
+        assertEq(priceFeed.getBalancesArrayExposed(), expectedBalances, "Incorrect balances");
+
+        assertEq(priceFeed.priceFeed0(), address(underlyingPriceFeeds[2]), "Incorrect priceFeed0");
+        assertEq(priceFeed.priceFeed1(), address(underlyingPriceFeeds[1]), "Incorrect priceFeed1");
+        assertEq(priceFeed.priceFeed2(), address(underlyingPriceFeeds[0]), "Incorrect priceFeed2");
+
+        assertEq(priceFeed.stalenessPeriod0(), 3 hours, "Incorrect stalenessPeriod0");
+        assertEq(priceFeed.stalenessPeriod1(), 2 hours, "Incorrect stalenessPeriod1");
+        assertEq(priceFeed.stalenessPeriod2(), 1 hours, "Incorrect stalenessPeriod2");
+    }
+
     // ------- //
     // HELPERS //
     // ------- //
@@ -99,8 +180,8 @@ contract BPTWeightedPriceFeedUnitTest is PriceFeedUnitTest {
         balancerVault.hackPoolTokens("TEST_POOL", _getPoolTokens(numAssets, 1.02 ether));
     }
 
-    function _newBalancerPriceFeed(uint256 numFeeds) internal returns (BPTWeightedPriceFeed) {
-        return new BPTWeightedPriceFeed(
+    function _newBalancerPriceFeed(uint256 numFeeds) internal returns (BPTWeightedPriceFeedHarness) {
+        return new BPTWeightedPriceFeedHarness(
             address(addressProvider),
             address(balancerVault),
             address(balancerPool),
@@ -129,7 +210,7 @@ contract BPTWeightedPriceFeedUnitTest is PriceFeedUnitTest {
     function _getUnderlyingPriceFeeds(uint256 numFeeds) internal view returns (PriceFeedParams[] memory priceFeeds) {
         priceFeeds = new PriceFeedParams[](numFeeds < 3 ? 2 : numFeeds);
         for (uint256 i; i < numFeeds; ++i) {
-            priceFeeds[i] = PriceFeedParams(address(underlyingPriceFeeds[i]), 1 days);
+            priceFeeds[i] = PriceFeedParams(address(underlyingPriceFeeds[i]), uint32(i + 1) * 1 hours);
         }
     }
 }
