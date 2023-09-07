7089ae24a890882a43aa4486087748bd74a4c199
Dmitry Lekhovitsky <lekhovitsky@gmail.com>
2023-09-07 13:35:32 +0300

    test: more advanced `BPTWeightedPriceFeed` tests

---
+++ contracts/test/unit/balancer/BPTWeightedPriceFeed.harness.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: UNLICENSED
+// Gearbox Protocol. Generalized leverage for DeFi protocols
+// (c) Gearbox Foundation, 2023.
+pragma solidity ^0.8.17;
+
+import {PriceFeedParams} from "../../../oracles/PriceFeedParams.sol";
+import {BPTWeightedPriceFeed} from "../../../oracles/balancer/BPTWeightedPriceFeed.sol";
+
+contract BPTWeightedPriceFeedHarness is BPTWeightedPriceFeed {
+    constructor(address addressProvider, address _vault, address _pool, PriceFeedParams[] memory priceFeeds)
+        BPTWeightedPriceFeed(addressProvider, _vault, _pool, priceFeeds)
+    {}
+
+    function getWeightsArrayExposed() external view returns (uint256[] memory weights) {
+        weights = _getWeightsArray();
+    }
+
+    function getBalancesArrayExposed() external view returns (uint256[] memory balances) {
+        balances = _getBalancesArray();
+    }
+}
