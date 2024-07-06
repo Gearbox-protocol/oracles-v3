5eddfd95c7f538d8bd16661413690f1d17ed94b4
0xmikko <26343374+0xmikko@users.noreply.github.com>
2024-07-06 21:15:38 +0200

    chore: removing aave & compound V2 feeds

--- contracts/interfaces/aave/IWAToken.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-// Gearbox Protocol. Generalized leverage for DeFi protocols
-// (c) Gearbox Foundation, 2023.
-pragma solidity ^0.8.23;
-
-interface IWAToken {
-    function exchangeRate() external view returns (uint256);
-}
